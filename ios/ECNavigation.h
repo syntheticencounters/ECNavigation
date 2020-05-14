78043ffd4d497647b676e257aa1bf20191efcf7a
syntheticencounters <36394356+syntheticencounters@users.noreply.github.com>
2020-05-13 20:08:59 -0500

    updates
    imported headers reverted back

--- ios/ECNavigation.h
+++ ios/ECNavigation.h
@@ -1,16 +1,6 @@
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
-#else
-#import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
-#endif
-
-#if __has_include(<MapboxCoreNavigation/MapboxCoreNavigation.h>)
 #import <MapboxCoreNavigation/MapboxCoreNavigation.h>
-#else
-#import "MapboxCoreNavigation.h"
-#endif
 
 @interface ECNavigation : RCTEventEmitter <RCTBridgeModule, NavigationServiceDelegate>
 
